520f0de1020b050a83ff1db315e241a28d24fbed
‘ITNBIZ-주문-안재영’ <ajy02@lgupluspartners.co.kr>
2022-04-09 22:39:52 +0900

    11111
    dasdf

---
+++ vuevuevue/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <HelloWorld msg="한권으로끝내는 vue3"/>
+  <Component>
+    <template v-slot="slotProps"> ✔️ {{ slotProps.item }} </template>
+  </Component>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Component from './components/Component.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Component,
+    
+  },
+  data() {
+    return {
+      msg: '초기 메시지',
+      msg2: '초기 메시지',
+    }
+  },
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuevuevue/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+/* eslint-disable vue/require-v-for-key */
+<template>
+  <ul style="list-style-type: none">
+    <li v-for="item in items" :key="item">
+      <slot :item="item"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data(){
+    return{
+      items:[1,2,3,],
+    }
+  },
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
