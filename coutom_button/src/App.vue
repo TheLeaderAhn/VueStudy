ef6526a565d1b529480e4736d69092c52b82e29c
‘ITNBIZ-주문-안재영’ <ajy02@lgupluspartners.co.kr>
2022-04-09 23:37:02 +0900

    button 기능
    버튼기능 완료

--- coutom_button/src/App.vue
+++ coutom_button/src/App.vue
@@ -1,21 +1,24 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <MyButton sm>My Button</MyButton>
+  <MyButton>My Button</MyButton>
+  <MyButton lg background-white>My Button</MyButton>
+  <MyButton sm pill text-red background-black>My Button</MyButton>
+  <MyButton md pill text-green background-pink>My Button</MyButton>
+  <MyButton lg pill @click.prevent="onClick">My Button</MyButton>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import MyButton from './components/MyButton.vue'
+
+export default {
+  name: 'App',
+  methods: {
+    onClick(evt) {
+      alert('Clicked')
+    },
+  },
+  components: {
+    MyButton,
+  },
 }
-</style>
+</script>
