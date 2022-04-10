85bc3f6cd83759b0dfb8360c0aad1986cf7666ba
‘ITNBIZ-주문-안재영’ <ajy02@lgupluspartners.co.kr>
2022-04-10 11:31:14 +0900

    gdgd
    gdgd

--- custom_ajy_table/src/App.vue
+++ custom_ajy_table/src/App.vue
@@ -1,21 +1,23 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <MyTable :header="header" :rows="rows" />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import MyTable from './components/MyTable.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      header: ['1번 제목', '2번 제목'],
+      rows: [
+        [{ data: '1번 내용' }, { data: '2번 내용' }],
+        [{ data: '1번 내용2', colspan: 2 }],
+      ],
+    }
+  },
+  components: {
+    MyTable,
+  },
 }
-</style>
+</script>
