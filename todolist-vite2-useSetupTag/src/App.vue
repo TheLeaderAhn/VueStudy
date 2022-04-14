c9dc95b48e432f0eff016734d7253376c45a4f3d
‘ITNBIZ-주문-안재영’ <ajy02@lgupluspartners.co.kr>
2022-04-14 17:25:47 +0900

    코드 setup으로 변경

--- todolist-vite2-useSetupTag/src/App.vue
+++ todolist-vite2-useSetupTag/src/App.vue
@@ -1,21 +1,35 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <header>
+    <hgroup class="my-5">
+      <h1>나의 할일</h1>
+      <em>{{ today }}</em>
+    </hgroup>
+  </header>
+  <todo-list-container />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<script>
+// SFC의 name, inheritAttrs, 사용자 옵션 등은 따로 script 태그에 작성한다.
+export default {
+  name: "App",
+}
+</script>
+
+<script setup>
+import { inject } from "vue"
+import TodoListContainer from "./components/TodoListContainer.vue"
+
+const today = inject("today")
+// <script setup> 에서는 따로 컴포넌트 선언이나 변수를 return하지 않아도 Vue 가 자동으로 설정해준다.
+</script>
+
+<style scoped>
+hgroup {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Arial Bold";
+}
+
+hgroup h1 {
+  font-weight: bolder;
 }
 </style>
